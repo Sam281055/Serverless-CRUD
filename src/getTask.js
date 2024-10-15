2b94cd00b67b37a8cb969441ca2fda7eae10a8c2
Sam281055 <sl281055@gmail.com>
2024-10-15 10:17:38 -0300

    First push

---
+++ src/getTask.js
@@ -0,0 +1,21 @@
+const AWS = require("aws-sdk");
+
+exports.getTask = async (event) => {
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const result = await dynamodb
+      .scan({
+        TableName: "TaskTable",
+      })
+      .promise();
+
+    const tasks = result.Items;
+    console.log(tasks);
+    return {
+      status: 200,
+      body: tasks,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
